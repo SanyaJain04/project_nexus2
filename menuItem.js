da80f25668d8b074b1bcff709114e69a85c10314
Sanya Jain <114746517+SanyaJain04@users.noreply.github.com>
2024-07-31 23:47:28 +0530

    Add files via upload

---
+++ menuItem.js
@@ -0,0 +1,470 @@
+
+let item=[
+    {
+      id:'1',
+      image: './images/paneerTikkaPizza.jpg',
+      item_name: 'Paneer Tikka Pizza',
+      original_price: 320,
+      rating: {
+          stars: 4.5,
+          count: 140,
+      },
+  },
+  
+  
+  {
+    id: '2',
+    image: './images/veggiedelight.avif',
+    item_name: 'Veggie Delight Pizza',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '3',
+    image: './images/farmhouse.jpg',
+    item_name: 'Farmhouse Pizza',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '4',
+    image: './images/cheesePizza.jpg',
+    item_name: 'Cheese Pizza',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '5',
+    image: './images/woodfire.avif',
+    item_name: 'Wood Fire Pizza',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  
+  },
+  
+  {
+    id: '6',
+    image: './images/veggiedelight.avif',
+    item_name: 'Veggie Delight Pizza',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  
+  {
+    id: '7',
+    image: './images/chocolateshake.jpg',
+    item_name: 'Chocolate Shake',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '8',
+    image: './images/coldcoffee.avif',
+    item_name: 'Cold Coffe',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '9',
+    image: './images/oreoshake.webp',
+    item_name: 'Oreo Shake',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '10',
+    image: './images/strawberry.jpg',
+    item_name: 'Strawberry Smoothie',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  
+  {
+    id: '11',
+    image: './images/idli.jpg',
+    item_name: 'Idli Sambhar',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '12',
+    image: './images/mysoremd.webp',
+    item_name: 'Mysore Masala Dosa',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '13',
+    image: './images/uttapam.jpg',
+    item_name: 'Uttapam',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '14',
+    image: './images/pastaws.jpeg',
+    item_name: 'White Sause Pasta',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '15',
+    image: './images/pastars.jpg',
+    item_name: 'Red Sause Pasta',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '16',
+    image: "./images/pinkpasta.jpeg",
+    item_name: 'Pink Sause Pasta',
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+
+  {
+    id: '17',
+    image: "./images/noodles.jpg",
+    item_name:'Schezwan Noodles' ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '18',
+    image: "./images/hakka.jpg" ,
+    item_name:'Hakka Noodles' ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '19',
+    image: "./images/garlic.jpeg",
+    item_name:"Chilli Garlic Noodles",
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '20',
+    image:"./images/chillipatato.jpg"  ,
+    item_name:"Chilli Potato" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '21',
+    image:"./images/garlicbread.jpg" ,
+    item_name: "Garlic Bread",
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '22',
+    image: "./images/chillipaneer.jpg",
+    item_name:"Chilli Paneer" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '23',
+    image:"./images/burger.jpg" ,
+    item_name: "Burger",
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '24',
+    image: "./images/kulcha.jpg",
+    item_name:"Amritsari Kulcha" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '25',
+    image: "./images/cb.cms",
+    item_name: "Chola Bhatura",
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+  {
+    id: '26',
+    image:"./images/lassi.jpeg" ,
+    item_name:"Lassi" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+
+  {
+    id: '27',
+    image: "./images/nachos.jpg",
+    item_name:"Cheese Nachos" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+  
+
+  {
+    id: '28',
+    image:"./images/nachos2.jpeg" ,
+    item_name:"Veggies Nachos" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+  {
+    id: '29',
+    image:"./images/tacos.webp",
+    item_name:"Taco" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+  {
+    id: '30',
+    image:"./images/juiceg.jpeg",
+    item_name:"Guava Juice" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+
+  {
+    id: '31',
+    image:"./images/juicep.jpeg",
+    item_name:"Pineapple Juice" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+
+  {
+    id: '32',
+    image:"./images/juiceo.jpg",
+    item_name:"Orange Juice" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+
+  {
+    id: '33',
+    image:"./images/sundae.jpg",
+    item_name:"Ice Cream Sundae" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+  {
+    id: '34',
+    image:"./images/cheesecakeb.jpeg",
+    item_name:"Blueberry Cheesecake" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '35',
+    image:"./images/prosecco.webp",
+    item_name:"Chocolate Sundae" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '36',
+    image:"./images/gulabjamun.jpeg",
+    item_name:"Gulab Jamun" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '37',
+    image:"./images/pastry.jpg",
+    item_name:"Pastries" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '38',
+    image:"./images/dal.jpg",
+    item_name:"Dal Tadka" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '39',
+    image:"./images/dal2.jpeg",
+    item_name:"Dal Makhani" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+
+  {
+    id: '40',
+    image:"./images/dal3.jpg",
+    item_name:"Dal Fry" ,
+    original_price: 320,
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+  },
+
+]
